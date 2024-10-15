bd16a99819475625267aea1fec03454889aa53bd
raya1222 <raya.mtpt@gmail.com>
2024-10-15 14:51:54 -0400

    hello

---
+++ open_close_timer.py
@@ -0,0 +1,13 @@
+# get the minute and hour inputs from user
+hour = input("please insert the hour of day it is in 24 hour time:  ")
+minute = input("please input what mintue of the day it is:  ")
+
+#making our conditions:
+
+# if the time is between 9am-18:30 its open
+if hour>=9 and hour<=18:
+    print("we are opened pay us a visit!")
+elif     minute=<29:
+    print("we are closing soon")
+else:
+    print("we are closed sorry:(")
